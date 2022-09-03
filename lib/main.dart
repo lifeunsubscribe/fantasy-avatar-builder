af448887dcfebe124d8966e42a97ec9e3deb7be4
sarah wadley <lifeunsubscribe@gmail.com>
2022-09-03 17:17:50 -0400

    updated font

--- lib/main.dart
+++ lib/main.dart
@@ -8,11 +8,17 @@ void main() => runApp(MaterialApp(
           style: TextStyle(
             fontSize: 30.0,
             fontWeight: FontWeight.bold,
+            letterSpacing: 1.5,
+            fontFamily: 'ShadowsIntoLight',
             color: Colors.brown[200]
           ),
         ),
         centerTitle: true,
         backgroundColor: Colors.brown[700],
+      ),
+      body: Center(
+        child: Text( "whatever"
+        ),
       ),
     )
   ));
